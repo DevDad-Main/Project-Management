ca532c6a592dccd62b58de5fe86f15def2e255b2
Oliver Metz <softwaredevdad@gmail.com>
2025-10-29 08:45:50 +0100

    add prisma.schema to backend with all models, add clerk keys to .env and add extra packages for prisma to talk to our neon database

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(clerkMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 app.get("/", (_, res) => {
   res.send("Server is live!");
 });
 
 app.listen(PORT || 3000, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
