65032d0f9846d72279e8b17ea2acdf078e6091b7
Oliver Metz <softwaredevdad@gmail.com>
2025-10-29 09:12:22 +0100

    add basic inngest setup and config file, where later we will add our custom functions to listen to clerk webhooks and other logic for sending emails to other users when we assign tasks, setp endpoint in server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,29 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
+import { inngest, functions } from "./inngest";
 
+//#region Constants
 const app = express();
 const PORT = process.env.PORT || 3000;
+//#endregion
 
+//#region Middleware
 app.use(cors());
 app.use(clerkMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//NOTE: Inngest Endpoint so it can listen to our events and webhooks and fire them off.
+app.use("/api/inngest", serve({ client: inngest, functions }));
+//#endregion
 
+//#region Route Endpoints
 app.get("/", (_, res) => {
   res.send("Server is live!");
 });
+//#endregion
 
 app.listen(PORT || 3000, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
